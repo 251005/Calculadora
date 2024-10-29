231a0d2868b3b90c305feac49c4e673f39657152
Leonardo Soares de Souza <106339564+251005@users.noreply.github.com>
2024-10-29 08:57:20 -0300

    Update JavaScript.js

--- Calculadora/JavaScript.js
+++ Calculadora/JavaScript.js
@@ -61,20 +61,7 @@
     }
         
 
-
-    while (condition) {
-        
-    }
-
-    do {
-        
-    } while (condition);
-
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
         
     }
-
-    array.forEach(element => {
-        
-    });
